e72fe978045e116073de05b3b7570ddad022379f
Federico-Farinella <federicofarinella1990gmail.com>
2023-06-17 21:32:07 +0200

    Modified Login

--- app/src/main/java/com/alfaproject/alfapizza/screens/Login.kt
+++ app/src/main/java/com/alfaproject/alfapizza/screens/Login.kt
@@ -1,6 +1,9 @@
 package com.alfaproject.alfapizza.screens
 
 import android.content.Context
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import androidx.compose.foundation.Image
 import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
@@ -21,8 +24,13 @@ import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -37,15 +45,38 @@ import com.alfaproject.alfapizza.R
 import com.alfaproject.alfapizza.ui.theme.BoxColor
 import com.alfaproject.alfapizza.ui.theme.MyTextFieldColor
 import com.alfaproject.alfapizza.screens.MyOutlinedTextField
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
+import com.google.gson.Gson
+import com.google.gson.JsonObject
+import kotlinx.coroutines.launch
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.Response
+import java.io.IOException
 
 @Composable
 fun LoginView() {
     val context: Context = LocalContext.current
+    val coroutineScope = rememberCoroutineScope()
     //var email by remember{mutableStateOf("") }
     val int = remember{mutableStateOf("")}
     val email = remember{mutableStateOf("")}
     val password = remember{ mutableStateOf("") }
     val code = remember{ mutableStateOf("") }
+    var firstTeacher: MutableState<JsonObject> = remember{ mutableStateOf(JsonObject()) }
+    if (!firstTeacher.value.entrySet().isEmpty())  {
+        //Toast.makeText(context, "name", Toast.LENGTH_SHORT).show()
+    Toast.makeText(context, firstTeacher.value.get("name").asString, Toast.LENGTH_SHORT).show()
+    }
+    //firstTeacher.addProperty("tel", 335)
 
     Column(modifier = Modifier
         //.fillMaxHeight()
@@ -93,11 +124,99 @@ fun LoginView() {
         //Spacer(modifier = Modifier.height(30.dp))
         //onClick = {Toast.makeText(context, "Stai provando il login", Toast.LENGTH_SHORT).show()}
 
-        Button(onClick = {if (email.value.isNotEmpty()) {
+        /*Button(onClick = {if (email.value.isNotEmpty()) {
             //LoggedView(context = context)
         // Continua da qui...
 
-        } },
+        } },*/
+        Button(onClick = {if (email.value.isNotEmpty()) {
+            //Toast.makeText(context, "name", Toast.LENGTH_SHORT).show()
+            //LoggedView(context = context)
+            // Prova (poi cancella tutto il Button)
+            //Trovo URL
+            //var firstTeacher: JsonObject
+            val storage = Firebase.storage
+            val storageRef = storage.reference
+            val fileRef = storageRef.child("esempio.json")
+
+            fileRef.downloadUrl
+                .addOnSuccessListener { uri ->
+                    val fileUrl = uri.toString()
+
+                    val client = OkHttpClient()
+                    val request = Request.Builder()
+                        .url(fileUrl)
+                        .build()
+
+                    client.newCall(request).enqueue(object : Callback {
+                        override fun onFailure(call: Call, e: IOException) {
+                            // Gestisci eventuali errori durante il download del file
+                            Toast.makeText(context, "error", Toast.LENGTH_SHORT).show()
+                            Log.d("File Download", "Error downloading file: ${e.message}")
+                            Log.e("File Download", "Error downloading file: ${e.message}")
+                        }
+
+                        override fun onResponse(call: Call, response: Response) {
+                            if (response.isSuccessful) {
+                                val json = response.body?.string()
+
+                                val gson = Gson()
+                                val jsonObject = gson.fromJson(json, JsonObject::class.java)
+                                //val jsonArray = jsonObject.getAsJsonArray("teachers")
+                                //firstTeacher = jsonArray[0].asJsonObject
+
+                                val jsonArray = jsonObject.getAsJsonArray("teachers")
+                                jsonArray[0].asJsonObject.addProperty("name", email.value)
+                                firstTeacher.value.addProperty("name", email.value)
+
+                                //firstTeacher.addProperty("name", email.value)
+                                Log.d("File Download", "JSON: $email.value\n$jsonObject\n${firstTeacher.value}")
+
+
+                                // Converti l'oggetto JSON modificato in una stringa JSON
+                                val modifiedJson = gson.toJson(jsonObject)
+
+                                // Crea una richiesta HTTP POST per caricare il file JSON modificato nel Firebase Storage
+                                val postRequest = Request.Builder()
+                                    .url(fileUrl) // Usa lo stesso URL del file originale
+                                    .post(modifiedJson.toRequestBody("application/json".toMediaTypeOrNull()))
+                                    .build()
+
+                                client.newCall(postRequest).enqueue(object : Callback {
+                                    override fun onFailure(call: Call, e: IOException) {
+                                        // Gestisci eventuali errori durante l'invio della richiesta POST
+                                        Log.e("File Upload", "Error uploading file: ${e.message}")
+                                    }
+
+                                    override fun onResponse(call: Call, response: Response) {
+                                        if (response.isSuccessful) {
+                                            // Il file JSON è stato modificato e caricato con successo
+                                            Log.d("File Upload", "File modified and uploaded successfully")
+                                        } else {
+                                            // Gestisci la risposta non riuscita
+                                            Log.e("File Upload", "Error response: ${response.code}")
+                                        }
+                                    }
+                                })
+
+                                // Ora puoi utilizzare l'oggetto JSON come desideri
+                                Log.d("File Download", "JSON: $jsonObject")
+                            } else {
+                                // Gestisci la risposta non riuscita
+                                Log.e("File Download", "Error response: ${response.code}")
+                            }
+                        }
+                    })
+                }
+                .addOnFailureListener { exception ->
+                    // Gestisci eventuali errori durante l'ottenimento dell'URL di download
+                    Log.e("Firebase Storage", "Error getting download URL: ${exception.message}")
+                }
+            //Toast.makeText(context, "name", Toast.LENGTH_SHORT).show()
+        //Toast.makeText(context, firstTeacher.get("name").asString, Toast.LENGTH_SHORT).show()
+        }
+            //firstTeacher.addProperty("tel", 335)
+                         },
             colors = ButtonDefaults.buttonColors(backgroundColor = BoxColor, contentColor = Color.White),
             modifier = Modifier
                 //.padding(50.dp)
